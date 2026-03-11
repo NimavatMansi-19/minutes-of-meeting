08a6665c67222f66919e5f1505c34e738b6ae4cb
Nimavat Mansi <mansinimavat86@gmail.com>
2026-03-11 13:02:49 +0530

    Added backend configuration with Aiven MySQL

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -10,20 +10,22 @@ import { AuthModule } from './modules/auth/auth.module';
     }),
 
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: 3306,
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-      }),
-    }),
-
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get<string>('DB_HOST'),
+    port: Number(configService.get<string>('DB_PORT')),
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+}),
     AuthModule,
   ],
 })
